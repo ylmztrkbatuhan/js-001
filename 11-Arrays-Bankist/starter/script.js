0c7239a72534916c8fabc6b0a9854bc64951bc1e
oboy21 <oboy11b@gmail.com>
2024-03-24 23:34:14 +0300

    checkogs changed!

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -195,4 +195,5 @@ const checkDogs = function (dogsJulia, dogsKate) {
   });
 };
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
