bbb6a9e40c7b5245a104d5fb2b08115bf681676f
oboy21 <oboy11b@gmail.com>
2024-04-29 22:08:05 +0300

    implementing login

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -120,6 +120,30 @@ const createUsernames = function (accs) {
 
 createUsernames(accounts);
 
+//Event handler
+
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  //Prevent form from submitting
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.computedStyleMap.opacity = 100;
+    //Display movements
+    //Display balance
+    //Display Summary
+  }
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
