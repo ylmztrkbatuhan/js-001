3631a4f836320b9ac3fd93648130229b135f961d
oboy21 <oboy11b@gmail.com>
2024-05-10 14:17:12 +0300

    working with big int

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -349,3 +349,47 @@ labelBalance.addEventListener('click', function () {
 });
 
 //Nth
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log('230_000');
+console.log('230_000');
+
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+console.log(858558786892947094702974092n);
+console.log(BigInt(85855878));
+
+//Operations
+
+console.log(10000n + 100000n);
+
+const huge = 75203758273489234868642n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == '20');
+
+console.log(huge + ' is Really big!!!');
+
+//Divisions
+console.log(10n / 3n);
+console.log(10 / 3);
