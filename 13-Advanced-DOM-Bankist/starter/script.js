3f64b21b278f6efa25dd8d24b51009f997410265
oboy21 <oboy11b@gmail.com>
2024-06-04 14:11:27 +0300

    passing arguments into handler

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,8 +1,18 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-/*
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  ///////////////////////////////////////
+  // Modal window
+  /*
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
@@ -80,48 +90,38 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // };
 */
 
-//Page navigation ////////////////////////////////////////////////////////////////
+  //Page navigation ////////////////////////////////////////////////////////////////
 
-// document.querySelectorAll('.nav__link').forEach(function (el) {
-//   el.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const id = this.getAttribute('href');
-//     console.log(id);
-//     document.querySelector(id).scrollIntoView({
-//       behavior: 'smooth',
-//     });
-//   });
-// });
+  // document.querySelectorAll('.nav__link').forEach(function (el) {
+  //   el.addEventListener('click', function (e) {
+  //     e.preventDefault();
+  //     const id = this.getAttribute('href');
+  //     console.log(id);
+  //     document.querySelector(id).scrollIntoView({
+  //       behavior: 'smooth',
+  //     });
+  //   });
+  // });
 
-// 1. Add event listener to common parent element
-// 2. determine what element originated the event
+  // 1. Add event listener to common parent element
+  // 2. determine what element originated the event
 
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-  console.log(e.target);
-  e.preventDefault();
+  document.querySelector('.nav__links').addEventListener('click', function (e) {
+    console.log(e.target);
+    e.preventDefault();
 
-  //Matching strategy
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
-    console.log(id);
-    document.querySelector(id).scrollIntoView({
-      behavior: 'smooth',
-    }),
-      console.log('LINK');
-  }
-});
+    //Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+      const id = e.target.getAttribute('href');
+      console.log(id);
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+      }),
+        console.log('LINK');
+    }
+  });
 
-// Tabbed component
-
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
-tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
-
-tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
+  // Tabbed component
 
   //Guard clause
   if (!clicked) return;
@@ -138,6 +138,41 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Menu fade animation
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {});
+
+nav.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = 1;
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+
+// Passing "argument" into handler
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
 //////////////////////////////////////////////////////////////////////////////////
 
 //Selecting elements
