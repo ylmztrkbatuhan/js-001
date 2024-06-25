7ef68d3e870a73552e3d6e967c6d44c473d842fa
oboy21 <oboy11b@gmail.com>
2024-06-25 20:42:51 +0300

    defer and async

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -465,3 +465,17 @@ const slider = function () {
   });
 };
 slider();
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree built!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded', e);
+});
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = '';
+// });
