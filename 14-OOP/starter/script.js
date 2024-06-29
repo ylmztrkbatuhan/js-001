e3ed398a34c5b70b2479bd3c190287dbc8590be5
oboy21 <oboy11b@gmail.com>
2024-06-29 11:25:31 +0300

    coding challenge 1

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -47,4 +47,46 @@ Person.prototype.species = 'Homo Sapiens';
 console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonasa.hasOwnProperty('species'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 5, 6, 9, 3]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going to at ${this.speed}km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
