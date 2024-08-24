ba1cef3a910ee13f5332455356b65d724b7ef9b9
oboy21 <oboy11b@gmail.com>
2024-08-24 19:44:21 +0300

    challenge 1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -20,12 +20,12 @@ const renderCountry = function (data, className = '') {
       </article>`;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  // countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 
 const renderError = function (msg) {
   countriesContainer.insertAdjacentText('beforeend', msg);
-  // countriesContainer.style.opacity = 1;
+  countriesContainer.style.opacity = 1;
 };
 /*
 const getCountryAndNeighbour = function (country) {
@@ -145,4 +145,31 @@ btn.addEventListener('click', function () {
   // getCountryData('portugal');
 });
 
-getCountryData('australia');
+// getCountryData('australia');
+
+/////////////////////Challenge 1///////////////////
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      console.log(res);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message}♥`));
+};
+
+whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
