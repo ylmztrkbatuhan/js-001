784bad06628b0463c0386a56a5e5d590184df933
oboy21 <oboy11b@gmail.com>
2024-09-15 18:33:41 +0300

    common js module

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -3,7 +3,9 @@
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('Importing module');
+import shoppingCart from './shoppingCart';
+
+// console.log('Importing module');
 //console.log(shippingCost);
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -22,13 +24,61 @@ console.log('Importing module');
 // console.log(data);
 // console.log('Something');
 
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
-  console.log(data);
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+//   console.log(data);
 
-  return { title: data.at(-1), text: data.at(-1).body };
-};
+//   return { title: data.at(-1), text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
-console.log(lastPost);
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+// // // Not very clean
+// // lastPost.then(last => console.log(last));
+
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+//     );
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
+
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
+
+
+// Export
+export.addToCart = function (product, quantity) {
+      cart.push({ product, quantity });
+      console.log(
+        `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+      );
+    };
+
+// Import
+const { addToCart } = require('.shoppingCart.js');
